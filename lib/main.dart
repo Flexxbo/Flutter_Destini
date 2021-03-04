107cc1acb7b83c859a18939c3453d36cd8778dc4
Flexxbo <flexxbo@gmail.com>
2021-03-04 18:25:13 +0100

    added storylogic

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,9 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 child: FlatButton(
                   onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
@@ -75,6 +78,9 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
                   },
                   color: Colors.blue,
                   child: Text(
